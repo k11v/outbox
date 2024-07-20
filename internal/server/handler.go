9c85e479df7537f46d82ba8e690de9e6388a232c
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-07-20 20:31:46 +0300

    Simplify handlers

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -5,8 +5,6 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
-
-	"github.com/google/uuid"
 )
 
 type handler struct {
@@ -29,48 +27,20 @@ func (h *handler) handleGetHealth(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type createMessageRequest struct {
-	Content string `json:"content"`
-}
-
-type createMessageResponse struct {
-	ID uuid.UUID `json:"id"`
-}
-
 func (h *handler) handleCreateMessage(w http.ResponseWriter, r *http.Request) {
 	defer closeWithLog(r.Body, h.log)
-	var req createMessageRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		resp := errorResponse{
-			Code:    "invalid_request",
-			Message: "invalid JSON",
-		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		if err = json.NewEncoder(w).Encode(resp); err != nil {
-			h.log.Error("failed to encode response", "error", err)
-		}
-		return
-	}
-
-	resp := createMessageResponse{ID: uuid.New()}
-
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		h.log.Error("failed to encode response", "error", err)
-	}
 }
 
 type getStatisticsResponse struct {
-	ProcessedMessages int `json:"processed_messages"`
+	Count int `json:"count"`
 }
 
 func (h *handler) handleGetStatistics(w http.ResponseWriter, r *http.Request) {
 	defer closeWithLog(r.Body, h.log)
 
-	resp := getStatisticsResponse{ProcessedMessages: 0}
+	resp := getStatisticsResponse{Count: 0}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -79,10 +49,10 @@ func (h *handler) handleGetStatistics(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type errorResponse struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
-}
+// type errorResponse struct {
+// 	Code    string `json:"code"`
+// 	Message string `json:"message"`
+// }
 
 func closeWithLog(c io.Closer, log *slog.Logger) {
 	if err := c.Close(); err != nil {
