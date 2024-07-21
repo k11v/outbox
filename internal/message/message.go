6ab255fa46566c49f83047ffe170d11812e4ac3c
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-07-21 17:01:05 +0300

    Add KafkaProducer

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,12 +1,46 @@
 package message
 
+import (
+	"context"
+	"github.com/segmentio/kafka-go"
+	"strings"
+)
+
 type Message struct {
 	Key   []byte
 	Value []byte
 }
 
-type Producer struct{}
+type Producer interface {
+	Produce(ctx context.Context, messages []Message) error
+}
 
-func (p *Producer) Produce(messages []Message) error {
+type KafkaProducer struct {
+	Writer *kafka.Writer // required
+}
+
+func (p *KafkaProducer) Produce(ctx context.Context, messages []Message) error {
+	kafkaMessages := make([]kafka.Message, len(messages))
+	for i, m := range messages {
+		kafkaMessages[i] = kafka.Message{
+			Key:   m.Key,
+			Value: m.Value,
+		}
+	}
+
+	err := p.Writer.WriteMessages(ctx, kafkaMessages...)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func NewKafkaWriter(addr string) *kafka.Writer {
+	addrs := strings.Split(addr, ",")
+	return &kafka.Writer{
+		Addr:         kafka.TCP(addrs...),
+		Balancer:     &kafka.LeastBytes{},
+		RequiredAcks: kafka.RequireOne,
+	}
 }
