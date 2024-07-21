23b29d0c95652bff6aa58a2997a027122776f4eb
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-07-21 15:46:42 +0300

    Add kafka-up implementation base

--- cmd/kafka-up/main.go
+++ cmd/kafka-up/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log/slog"
+	"net"
 	"os"
+	"strconv"
+
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
@@ -14,5 +20,50 @@ func main() {
 }
 
 func run() error {
+	log := slog.Default()
+
+	topic := "example"
+	addr := "kafka:9092"
+
+	if err := createTopic(topic, addr, log); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func createTopic(topic, addr string, log *slog.Logger) error {
+	conn, err := kafka.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
+	defer closeWithLog(conn, log)
+
+	controller, err := conn.Controller()
+	if err != nil {
+		return err
+	}
+
+	controllerConn, err := kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		return err
+	}
+	defer closeWithLog(controllerConn, log)
+
+	topicCfg := kafka.TopicConfig{
+		Topic:             topic,
+		NumPartitions:     1,
+		ReplicationFactor: 1,
+	}
+	if err = controllerConn.CreateTopics(topicCfg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func closeWithLog(c io.Closer, log *slog.Logger) {
+	if err := c.Close(); err != nil {
+		log.Error("failed to close", "error", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/k11v/squeak
 
 go 1.22.5
 
-require github.com/caarlos0/env/v11 v11.1.0
+require (
+	github.com/caarlos0/env/v11 v11.1.0
+	github.com/segmentio/kafka-go v0.4.47
+)
 
 require (
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
-	github.com/segmentio/kafka-go v0.4.47 // indirect
 )
