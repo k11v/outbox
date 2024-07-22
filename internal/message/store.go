c415de68662ede5d452bb0e43734a81db4e9d995
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-07-22 09:58:08 +0300

    Add ctx parameter to Store functions

--- internal/message/store.go
+++ internal/message/store.go
@@ -1,6 +1,8 @@
 package message
 
 import (
+	"context"
+
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -8,13 +10,13 @@ import (
 // Store provides an interface for storing and retrieving messages.
 type Store interface {
 	// Add adds one or more messages to the store.
-	Add(messages ...Message) error
+	Add(ctx context.Context, messages ...Message) error
 
 	// GetUndelivered returns a list of undelivered messages.
-	GetUndelivered() ([]Message, error)
+	GetUndelivered(ctx context.Context) ([]Message, error)
 
 	// SetDelivered marks the messages as delivered.
-	SetDelivered(ids ...uuid.UUID) error
+	SetDelivered(ctx context.Context, ids ...uuid.UUID) error
 }
 
 // PostgresStore implements the Store interface using a PostgreSQL database.
@@ -22,14 +24,14 @@ type PostgresStore struct {
 	Pool *pgxpool.Pool
 }
 
-func (s *PostgresStore) Add(messages ...Message) error {
+func (s *PostgresStore) Add(ctx context.Context, messages ...Message) error {
 	return nil
 }
 
-func (s *PostgresStore) GetUndelivered() ([]Message, error) {
+func (s *PostgresStore) GetUndelivered(ctx context.Context) ([]Message, error) {
 	return nil, nil
 }
 
-func (s *PostgresStore) SetDelivered(ids ...uuid.UUID) error {
+func (s *PostgresStore) SetDelivered(ctx context.Context, ids ...uuid.UUID) error {
 	return nil
 }
