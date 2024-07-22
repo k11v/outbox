f605cf9eae62016affb7adfc42fb99652288254e
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-07-22 09:56:46 +0300

    Add PostgresStore base

--- internal/message/store.go
+++ internal/message/store.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // Store provides an interface for storing and retrieving messages.
@@ -14,4 +15,21 @@ type Store interface {
 
 	// SetDelivered marks the messages as delivered.
 	SetDelivered(ids ...uuid.UUID) error
+}
+
+// PostgresStore implements the Store interface using a PostgreSQL database.
+type PostgresStore struct {
+	Pool *pgxpool.Pool
+}
+
+func (s *PostgresStore) Add(messages ...Message) error {
+	return nil
+}
+
+func (s *PostgresStore) GetUndelivered() ([]Message, error) {
+	return nil, nil
+}
+
+func (s *PostgresStore) SetDelivered(ids ...uuid.UUID) error {
+	return nil
 }
