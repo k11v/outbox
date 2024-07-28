d89155ba7a0c7f9eebec046c8b4bded6c23b02ae
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-07-28 14:35:22 +0300

    Add created_at to outbox

--- cmd/postgres-up/migrations/0_init.up.sql
+++ cmd/postgres-up/migrations/0_init.up.sql
@@ -4,10 +4,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS outbox (
     id uuid DEFAULT uuid_generate_v4(),
+    created_at timestamp with time zone DEFAULT now(),
+
+    -- Message
     topic text NOT NULL,
     key bytea NOT NULL,
     value bytea NOT NULL,
-    headers jsonb NOT NULL, -- e.g. [{"key": "Content-Type", "value": "YXBwbGljYXRpb24vanNvbg=="}]
+    headers jsonb NOT NULL, -- e.g. [{"key": "Content-Type", "value_base64": "YXBwbGljYXRpb24vanNvbg=="}]
+
     PRIMARY KEY (id)
 );
 
