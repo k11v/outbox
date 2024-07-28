1bf0464a2fe80185f1d4764d64b698c52cc9037e
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-07-28 14:36:57 +0300

    Add status to outbox

--- cmd/postgres-up/migrations/0_init.up.sql
+++ cmd/postgres-up/migrations/0_init.up.sql
@@ -5,6 +5,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS outbox (
     id uuid DEFAULT uuid_generate_v4(),
     created_at timestamp with time zone DEFAULT now(),
+    status text NOT NULL,
 
     -- Message
     topic text NOT NULL,
@@ -12,7 +13,8 @@ CREATE TABLE IF NOT EXISTS outbox (
     value bytea NOT NULL,
     headers jsonb NOT NULL, -- e.g. [{"key": "Content-Type", "value_base64": "YXBwbGljYXRpb24vanNvbg=="}]
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CHECK (status IN ('pending', 'failed', 'succeeded'))
 );
 
 COMMIT;
