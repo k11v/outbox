075caebd98eecdaa57d991636ab35b9fb55d54ee
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-08-01 17:42:29 +0300

    Add message_infos table

--- cmd/postgres-up/migrations/0_init.up.sql
+++ cmd/postgres-up/migrations/0_init.up.sql
@@ -2,6 +2,11 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE IF NOT EXISTS message_infos (
+    id uuid DEFAULT uuid_generate_v4(),
+    value_length integer NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS outbox_messages (
     id uuid DEFAULT uuid_generate_v4(),
     created_at timestamp with time zone DEFAULT now(),
